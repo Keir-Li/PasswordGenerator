9537739343f92c9990da3363b8db43dc30dea53e
keir <keir.li@doverport.co.uk>
2022-01-04 19:05:00 +0000

    Add project files.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

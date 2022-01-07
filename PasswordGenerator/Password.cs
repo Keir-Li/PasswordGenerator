f9c68422aa40d0c731344bedd86b86b037c9185a
keir <keir.li@doverport.co.uk>
2022-01-07 11:53:03 +0000

    Initial - Program validation, Password Class and Password Generate method created.

---
+++ PasswordGenerator/Password.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    public class Password
+    {
+
+        public static string Generate(int passwordLength)
+        {
+            // Password variables
+            string lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm";
+            string capitalLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string numbers = "0123456789";
+            string specialCharacters = "!@#$%^&*()-_=+<>";
+            string allCharacters = lowercaseLetters + capitalLetters + numbers + specialCharacters;
+            string generatedPassword = "";
+
+            // New random number generator instance
+            Random newCharacter = new Random();
+
+            // Loop for the password length, each character to be random
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int randomNumber = newCharacter.Next(0, allCharacters.Length);
+                generatedPassword += allCharacters[randomNumber];
+            }
+
+            return generatedPassword;
+
+        }
+    }
+}
--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -6,7 +6,51 @@ namespace PasswordGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int passwordLengthMin = 5;
+            int passwordLengthMax = 100;
+            int passwordLength = 0;
+            var loop = true;
+
+
+            // Operning Text
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Password Generator");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("----------------------------------------------------------");
+
+            // Infinite Loop over the Program
+            while (loop == true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please enter a password length you'd like.");
+                Console.WriteLine("MIN: 5 characters");
+                Console.WriteLine("MAX: 100 characters");
+                passwordLength = int.Parse(Console.ReadLine());
+
+                // Password Min & Max Length Validation
+                if (passwordLength < passwordLengthMin || passwordLength > passwordLengthMax)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("ATTENTION!");
+                    Console.WriteLine("Length must be atleast 5 & less than 100 characters.");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    var password = Password.Generate(passwordLength);
+                    Console.WriteLine("----------------------------------------------------------");
+                    Console.WriteLine("----------------------------------------------------------");
+                    Console.WriteLine("Generated Password: " + password);
+                    Console.WriteLine("----------------------------------------------------------");
+                    Console.WriteLine("----------------------------------------------------------");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+
+            }
         }
+
     }
 }
